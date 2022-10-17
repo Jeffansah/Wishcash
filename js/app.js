0bf9b25ce12e910a49118bf42d67664a670dc4ec
Jeffrey Owusu Ansah <owusuansaj@coventry.ac.uk>
2022-10-17 17:30:48 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const menuToggler = document.querySelector(".Menu_button");
+const navbar = document.querySelector(".navbar");
+const collapsableMenu = document.querySelector(".collapsable_menu");
+
+
+menuToggler.addEventListener("click", () => {
+    navbar.classList.toggle("show-menu");
+    if(navbar.classList.contains("show-menu")){
+        collapsableMenu.style.maxHeight = collapsableMenu.scrollHeight + "px";
+        
+        setTimeout(() =>{
+            collapsableMenu.style.overflow = "visible";
+        }, 300
+        )}
+    else{
+        collapsableMenu.style.maxHeight = "0px";
+        collapsableMenu.style.overflow = "hidden";
+        }
+    });
+  
